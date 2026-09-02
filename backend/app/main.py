adfd239ef3a651e69b97fa6273162aae6a39f747
Jmartinezb11 <jmartinezb11@miumg.edu.gt>
2026-09-02 16:49:47 -0600

    chore: crear estructura inicial del proyecto

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Sistema de Gestión de Becas",
+    description="API del proyecto ADS",
+    version="1.0.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "API del Sistema de Gestión de Becas funcionando"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
